d4f8a0b95f759b518ab32da930c6050996062a3f
Ilayabharathi Sakthivel <ibharathi385@gmail.com>
2025-08-03 12:07:30 +0530

    Added APIFeatures class

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,9 +1,20 @@
 const Tour = require(`${__dirname}/../models/tourModel`);
 
-exports.getAllTours = async (req, res) => {
-  try {
-    // filtering
-    const queryObj = { ...req.query };
+exports.aliasTopTours = async (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
+class APIFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
+
+  filter() {
+    const queryObj = { ...this.queryString };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
@@ -14,28 +25,99 @@ exports.getAllTours = async (req, res) => {
       (match) => `$${match}`,
     );
 
-    // build query
-    let query = Tour.find(JSON.parse(queryString));
+    this.query.find(JSON.parse(queryString));
+    return this;
+  }
 
-    // const query = await Tour.find()
-    //   .where('duration')
-    //   .equals(5)
-    //   .where('difficulty')
-    //   .equals('easy');
-
-    // sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(',').join(' ');
+      this.query = this.query.sort(sortBy);
       //sort('price ratingsAverage')
     } else {
-      query = query.sort('-createdAt');
+      this.query = this.query.sort('-createdAt');
     }
+    return this;
+  }
 
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
+      //select('name duration difficulty price')
+    } else {
+      // Skip mangoose default fields
+      this.query = this.query.select('-__v');
+    }
+    return this;
+  }
+
+  paginate() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    this.query.skip(skip).limit(limit);
+    return this;
+  }
+}
+
+exports.getAllTours = async (req, res) => {
+  try {
+    // filtering
+    // const queryObj = { ...req.query };
+    // const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    // excludedFields.forEach((el) => delete queryObj[el]);
+
+    // // advanced filtering
+    // let queryString = JSON.stringify(queryObj);
+    // queryString = queryString.replace(
+    //   /\b(gte|gt|lte|lt)\b/g,
+    //   (match) => `$${match}`,
+    // );
+
+    // // build query
+    // let query = Tour.find(JSON.parse(queryString));
+
+    // sorting
+    // if (req.query.sort) {
+    //   const sortBy = req.query.sort.split(',').join(' ');
+    //   query = query.sort(sortBy);
+    //   //sort('price ratingsAverage')
+    // } else {
+    //   query = query.sort('-createdAt');
+    // }
     // { difficulty: 'easy', duration: { $gte: '4' } }
 
+    // field limiting
+    // if (req.query.fields) {
+    //   const fields = req.query.fields.split(',').join(' ');
+    //   query = query.select(fields);
+    //   //select('name duration difficulty price')
+    // } else {
+    //   // Skip mangoose default fields
+    //   query = query.select('-__v');
+    // }
+
+    // Pagination
+    // const page = req.query.page * 1 || 1;
+    // const limit = req.query.limit * 1 || 100;
+    // const skip = (page - 1) * limit;
+    // query.skip(skip).limit(limit);
+
+    // if (req.query.page) {
+    //   const numOfTours = await Tour.countDocuments();
+    //   if (skip >= numOfTours) {
+    //     throw new Error("This page doesn't exists");
+    //   }
+    // }
+
     // execute the query
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
 
     res.status(200).json({
       status: 'success',
@@ -49,7 +131,7 @@ exports.getAllTours = async (req, res) => {
     console.log(err);
     res.status(400).json({
       status: 'failure',
-      message: 'Ivalid data set',
+      message: 'Ivalid data sent',
     });
   }
 };
@@ -83,7 +165,7 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'failure',
-      message: 'Ivalid data set',
+      message: 'Ivalid data sent',
     });
   }
 };
@@ -104,7 +186,7 @@ exports.updateTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'failure',
-      message: 'Ivalid data set',
+      message: 'Ivalid data sent',
     });
   }
 };
@@ -119,7 +201,7 @@ exports.deleteTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'failure',
-      message: 'Ivalid data set',
+      message: 'Ivalid data sent',
     });
   }
 };
